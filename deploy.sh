69b1f22e1ef8ad97f3fa42e1b1c9d85aeb52cfb6
sillyhat <xushikuan1@163.com>
2019-11-17 02:33:56 +0800

    Merge branch 'master' of https://github.com/sillyhatxu/sillyhat-docker-elk
    # Conflicts:
    #	.gitignore

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,9 @@
-docker stack deploy -c docker-compose.yml elk
+docker service rm sillyhat_elasticsearch sillyhat_kibana sillyhat_logstash
+docker stack deploy -c docker-compose.yml sillyhat
 
+docker service update --force sillyhat_logstash
+
+docker service rm sillyhat_logstash
+docker stack deploy -c docker-compose.yml sillyhat
 
 sudo bin/elasticsearch-plugin install logstash-output-slack
